6b132e1f5de840b2c6a15fd9d7b6f4ce3ee1588c
Mayukhjit chakraborty <90026571+mayukh14Qf@users.noreply.github.com>
2024-08-14 14:24:03 +0530

    Add files via upload

---
+++ Day 3/Constructors.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMIT_NET.Day_3
+{
+
+    class Marvel
+    {
+        public String MovieName;
+        public String Collection;
+        public int NoOfCharacters;
+
+        // Deafult Constructor
+        public Marvel()
+        {
+            Console.WriteLine("hello I am in default Constructor");
+        }
+
+        // parameterized constructors
+
+        public Marvel(String MovieName, String Collection, int NoOfCharacters)
+        {
+            this.MovieName = MovieName;
+            this.Collection = Collection;
+            this.NoOfCharacters = NoOfCharacters;
+            Console.WriteLine("In Parameterized contructors");
+        }
+
+        public Marvel(int f)
+        {
+            Console.WriteLine("your Fan Score is "+f);
+        }
+
+
+        // Copy Constructrors
+
+        public Marvel(Marvel m)
+        {
+            this.MovieName=m.MovieName;
+            this.Collection = m.Collection;
+            this.NoOfCharacters=m.NoOfCharacters;
+            Console.WriteLine("In Copy Constructor");
+        }
+
+
+
+        public void print()
+        {
+            Console.WriteLine(MovieName);
+            Console.WriteLine(Collection);
+            Console.WriteLine(NoOfCharacters);
+        }
+
+    }
+
+
+
+    public class Constructors
+    {
+        static void Main(string[] args)
+        {
+            Marvel obj1= new Marvel();
+            obj1.MovieName = "Avengers";
+            obj1.NoOfCharacters = 50;
+            obj1.Collection = "2B";
+            obj1.print();
+
+            Marvel obj2=new Marvel("Deadpool","1B",9);
+            obj2.print();
+
+            Marvel obj3 = new Marvel(6);
+
+            Marvel obj4= new Marvel(obj1);
+            obj4.print();
+
+        }
+    }
+}
--- Day 3/oops.cs
+++ Day 3/oops.cs
@@ -1,30 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GMIT_NET.Day_3
 {
-    class father
-    {
-        public int age;
-        public string name;
 
-        public void identity()
+    class Language
+    {
+        public string lName;
+
+        public void lDescription()
         {
-            Console.WriteLine($"your name is {this.name} and age is {this.age}");
+            Console.WriteLine("your language is : "+this.lName);
         }
+    }
+
+
+    class Student
+    {
+        // properties
+        // instance variable
+        public string name;
+        public int rollNo;
+        public string email;
+        public static int age;
+
+        // functions
+        public void getData()
+        {
+            Console.WriteLine(this.name);
+            Console.WriteLine(this.rollNo);
+            Console.WriteLine(this.email);
+            Console.WriteLine(age);
+        }
+
+        public string uniRollNo()
+        {
+            return "2024/"+this.rollNo;
+        }
+
+        public static void admission()
+        {
+            Console.WriteLine("your age during admission is : " + age);
+            //Console.WriteLine(name); // we can not use non-static variable inside a static method
+        }
+
     }
 
     public class oops
     {
         static void Main(string[] args)
         {
-            father obj1= new father();
-            obj1.age = 45;
-            obj1.name = "xyz";
-            obj1.identity();
+           Student obj1= new Student();// object creation
+
+            Student.age = 23; // by calling the class name 
+            Student.admission();
+
+            obj1.name = "Mayukhjit";
+            obj1.rollNo = 54;
+            obj1.email = "mayukhc55@gmail.com";
+            
+            Console.WriteLine(obj1.uniRollNo());
+            obj1.getData();
+
+            Student obj2= new Student(); // obj2 => reference variable
+            obj2.name = "xyz";
+            obj2.rollNo = 14;
+            obj2.email = "abc@gmail.com";
+            obj2.getData();
+
+            Language lan1= new Language();
+            lan1.lName = "English";
+
+            lan1.lDescription();
+
+            
+
         }
     }
 }
