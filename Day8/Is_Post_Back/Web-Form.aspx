c573566237dc3fea61f3e067f34841742de9510e
Mayukhjit chakraborty <90026571+mayukh14Qf@users.noreply.github.com>
2024-08-27 21:35:59 +0530

    Add files via upload

---
+++ Day8/Is_Post_Back/Web-Form.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Web-Form.aspx.cs" Inherits="Is_Post_Back.Web_Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="127px" Width="116px"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="CLICK" />
+            <br />
+            <br />
+            <br />
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Day8/Is_Post_Back/Web-Form.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Is_Post_Back
+{
+    public partial class Web_Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Response.Write("We are Using IsPostBack");
+                AddListBoxItems();
+            }
+        }
+
+        void AddListBoxItems()
+        {
+            ListBox1.Items.Add("Item1");
+            ListBox1.Items.Add("Item2");
+            ListBox1.Items.Add("Item3");
+            ListBox1.Items.Add("Item4");
+        }
+    }
+}
