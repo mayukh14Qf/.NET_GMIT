cd2df5e488ddc81f1cb7f6c31195932bf7a1077d
Mayukhjit chakraborty <90026571+mayukh14Qf@users.noreply.github.com>
2024-09-06 17:23:17 +0530

    Add files via upload

---
+++ Day 13/DropDownList/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropDownList.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="20px" Width="124px">
+                <asp:ListItem Value="-1">Select City</asp:ListItem>
+                <asp:ListItem Value="1">Hyderabad</asp:ListItem>
+                <asp:ListItem Value="2">Kolkata</asp:ListItem>
+                <asp:ListItem Value="3">Odisha</asp:ListItem>
+                <asp:ListItem Value="4" Text="Pune"></asp:ListItem>
+
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="SUBMIT" OnClick="Button1_Click" />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Day 13/DropDownList/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (!IsPostBack)
+            //{
+            //    ListItem l1 = new ListItem("Chennai", "5");
+            //    ListItem l2 = new ListItem("Mumbai", "6");
+            //    ListItem l3 = new ListItem("Delhi", "7", false);
+
+            //    DropDownList1.Items.Add(l1);
+            //    DropDownList1.Items.Add(l2);
+            //    DropDownList1.Items.Add(l3);
+            //    DropDownList1.Items[4].Selected = true;
+            //}
+
+            if (!IsPostBack)
+            {
+                DropDownList1.SelectedValue = "2";
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedItem.Value == "-1")
+            {
+                Response.Write("Select A City");
+            }
+            else
+            {
+                
+                Response.Write("your Selected text :" + DropDownList1.SelectedItem.Text +"<br>");
+                Response.Write("your Selected Value :" + DropDownList1.SelectedItem.Value + "<br>");
+                Response.Write("your Selected Index :" + DropDownList1.SelectedIndex + "<br>");
+            }
+        }
+    }
+}
